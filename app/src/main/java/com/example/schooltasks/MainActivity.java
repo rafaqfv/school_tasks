54ff593f27691e6481e839309dd9259af340aa25
Rafael <127614067+rafaqfv@users.noreply.github.com>
2024-07-20 02:50:49 -0300

    Refatorar cadastro de usuário

--- app/src/main/java/com/example/schooltasks/MainActivity.java
+++ app/src/main/java/com/example/schooltasks/MainActivity.java
@@ -37,75 +37,92 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-
         mAuth = FirebaseAuth.getInstance();
-
         binding.backBtn.setOnClickListener(v -> finish());
 
         binding.btnSalvar.setOnClickListener(v -> {
             binding.progressBar2.setVisibility(View.VISIBLE);
-
-            String nome = binding.nomeInput.getText().toString().trim();
-            String telefone = binding.telefoneInput.getText().toString().trim();
-            String email = binding.emailInput.getText().toString().trim();
-            String senha = binding.senhaInput.getText().toString().trim();
-
-            if (!nome.isEmpty() && !telefone.isEmpty() && !email.isEmpty() && senha.length() >= 8) {
-                mAuth.createUserWithEmailAndPassword(email, senha)
-                        .addOnCompleteListener(task -> {
-                            if (task.isSuccessful()) {
-                                FirebaseUser user = task.getResult().getUser();
-                                Map<String, Object> userMap = new HashMap<>();
-                                userMap.put("Nome", nome);
-                                userMap.put("Telefone", telefone);
-                                userMap.put("Email", email);
-
-                                db.collection("users").document(user.getUid()).set(userMap)
-                                        .addOnSuccessListener(documentReference -> {
-                                            Toast.makeText(this, "Usuário salvo com sucesso", Toast.LENGTH_SHORT).show();
-                                            binding.progressBar2.setVisibility(View.INVISIBLE);
-                                            new Handler(getMainLooper()).postDelayed(() -> {
-                                                finish();
-                                                startActivity(new Intent(this, LoginActivity.class));
-                                            }, 1500);
-                                        })
-                                        .addOnFailureListener(e -> {
-                                            Toast.makeText(this, "Erro ao salvar usuário", Toast.LENGTH_SHORT).show();
-                                            binding.progressBar2.setVisibility(View.INVISIBLE);
-                                        });
-                            } else {
-                                Exception e = task.getException();
-                                Toast.makeText(this, "Erro: " + e.getMessage(), Toast.LENGTH_SHORT).show();
-                                binding.progressBar2.setVisibility(View.INVISIBLE);
-                            }
-                        });
-            }
-
-            if (nome.isEmpty()) {
-                binding.nomeInput.setError("Nome vazio.");
-                new Handler(getMainLooper()).postDelayed(() -> {
-                    binding.nomeInput.setError(null);
-                }, 2000);
-            }
-            if (telefone.isEmpty()) {
-                binding.telefoneInput.setError("Telefone Vazio.");
-                new Handler(getMainLooper()).postDelayed(() -> {
-                    binding.telefoneInput.setError(null);
-                }, 2000);
-            }
-            if (email.isEmpty()) {
-                binding.emailInput.setError("Email vazio.");
-                new Handler(getMainLooper()).postDelayed(() -> {
-                    binding.emailInput.setError(null);
-                }, 2000);
-            }
-            if (senha.length() < 8) {
-                binding.senhaInput.setError("Senha menor do que 8 dígitos.");
-                new Handler(getMainLooper()).postDelayed(() -> {
-                    binding.senhaInput.setError(null);
-                }, 2000);
+            if (validateFields()) {
+                cadastraUsuarioAuth();
             }
             binding.progressBar2.setVisibility(View.INVISIBLE);
         });
+    }
+
+    private void addUserDoc(FirebaseUser user) {
+        String nome = binding.nomeInput.getText().toString().trim();
+        String telefone = binding.telefoneInput.getText().toString().trim();
+        String email = binding.emailInput.getText().toString().trim();
+
+        Map<String, Object> userMap = new HashMap<>();
+        userMap.put("Nome", nome);
+        userMap.put("Telefone", telefone);
+        userMap.put("Email", email);
+
+        db.collection("users").document(user.getUid()).set(userMap)
+                .addOnSuccessListener(documentReference -> {
+
+                    Toast.makeText(this, "Usuário salvo com sucesso", Toast.LENGTH_SHORT).show();
+                    binding.progressBar2.setVisibility(View.INVISIBLE);
+
+                    new Handler(getMainLooper()).postDelayed(() -> {
+                        finish();
+                        startActivity(new Intent(this, LoginActivity.class));
+                    }, 1500);
+
+                })
+                .addOnFailureListener(e -> {
+                    Toast.makeText(this, "Erro ao salvar usuário", Toast.LENGTH_SHORT).show();
+                    binding.progressBar2.setVisibility(View.INVISIBLE);
+                });
+    }
+
+    private void cadastraUsuarioAuth() {
+        if (validateFields()) {
+
+            String email = binding.emailInput.getText().toString().trim();
+            String senha = binding.senhaInput.getText().toString().trim();
+
+            mAuth.createUserWithEmailAndPassword(email, senha)
+                    .addOnCompleteListener(task -> {
+                        if (task.isSuccessful()) {
+                            FirebaseUser user = task.getResult().getUser();
+                            addUserDoc(user);
+                        } else {
+                            Exception e = task.getException();
+                            Toast.makeText(this, "Erro: " + e.getMessage(), Toast.LENGTH_SHORT).show();
+                            binding.progressBar2.setVisibility(View.INVISIBLE);
+                        }
+                    });
+        }
+    }
+
+    private boolean validateFields() {
+        String nome = binding.nomeInput.getText().toString().trim();
+        String telefone = binding.telefoneInput.getText().toString().trim();
+        String email = binding.emailInput.getText().toString().trim();
+        String senha = binding.senhaInput.getText().toString().trim();
+
+        if (!nome.isEmpty() && !telefone.isEmpty() && !email.isEmpty() && senha.length() >= 8) {
+            return true;
+        }
+
+        if (nome.isEmpty()) {
+            binding.nomeInput.setError("Nome vazio.");
+            binding.nomeInput.setError(null);
+        }
+        if (telefone.isEmpty()) {
+            binding.telefoneInput.setError("Telefone Vazio.");
+            binding.telefoneInput.setError(null);
+        }
+        if (email.isEmpty()) {
+            binding.emailInput.setError("Email vazio.");
+            binding.emailInput.setError(null);
+        }
+        if (senha.length() < 8) {
+            binding.senhaInput.setError("Senha menor do que 8 dígitos.");
+            binding.senhaInput.setError(null);
+        }
+        return false;
     }
 }

1e19cff9f373dc8fe44d3c7d85ebf6adaa0410cb
Rafael <127614067+rafaqfv@users.noreply.github.com>
2024-07-24 02:10:36 -0300

    Criado funcionalidade de update e delete

--- app/src/main/java/com/example/schooltasks/UpdateTask.java
+++ app/src/main/java/com/example/schooltasks/UpdateTask.java
@@ -2,6 +2,7 @@ package com.example.schooltasks;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,13 +12,19 @@ import androidx.core.view.WindowInsetsCompat;
 
 import com.example.schooltasks.databinding.ActivityUpdateTaskBinding;
 import com.google.android.material.datepicker.MaterialDatePicker;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.Locale;
+import java.util.Map;
 
 public class UpdateTask extends AppCompatActivity {
     private ActivityUpdateTaskBinding binding;
+    private FirebaseFirestore db;
+    private Intent intent;
+    private String id;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,29 +37,55 @@ public class UpdateTask extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-
+        db = FirebaseFirestore.getInstance();
         binding.backBtn.setOnClickListener(v -> finish());
+        intent = getIntent();
+        updateFields();
         datePicker();
-
-        Intent intent = getIntent();
-        String disciplina = intent.getStringExtra("disciplina");
-        String titulo = intent.getStringExtra("titulo");
-        String dataDeEntrega = intent.getStringExtra("data");
-        String descricao = intent.getStringExtra("descricao");
-        String id = intent.getStringExtra("id");
-
-        binding.data.setText(dataDeEntrega);
-        binding.disciplina.setText(disciplina);
-        binding.titulo.setText(titulo);
-        binding.descricao.setText(descricao);
-
-
+        binding.salvarButton.setOnClickListener(v -> updateTask(id));
+        binding.excluirBtn.setOnClickListener(v -> deleteTask(id));
     }
 
-    public void updateTask() {
+    private void deleteTask(String id) {
+        db.collection("tasks").document(id)
+                .delete()
+                .addOnSuccessListener(aVoid -> {
+                    Toast.makeText(this, "Tarefa excluída com sucesso!", Toast.LENGTH_SHORT).show();
+                    finish();
+                })
+                .addOnFailureListener(e -> {
+                    Toast.makeText(this, "Erro ao excluir tarefa.", Toast.LENGTH_SHORT).show();
+                });
+    }
 
+    private void updateTask(String id) {
+        // Obtenha os dados atualizados das views
+        String updatedDisciplina = binding.disciplina.getText().toString();
+        String updatedTitulo = binding.titulo.getText().toString();
+        String updatedDataDeEntrega = binding.data.getText().toString();
+        String updatedDescricao = binding.descricao.getText().toString();
 
+        if (updatedDisciplina.isEmpty() || updatedTitulo.isEmpty() || updatedDataDeEntrega.isEmpty() || updatedDescricao.isEmpty()) {
+            Toast.makeText(this, "Por favor, preencha todos os campos.", Toast.LENGTH_SHORT).show();
+            return;
+        }
 
+        Map<String, Object> task = new HashMap<>();
+        task.put("disciplina", updatedDisciplina);
+        task.put("titulo", updatedTitulo);
+        task.put("dataDeEntrega", updatedDataDeEntrega);
+        task.put("descricao", updatedDescricao);
+
+        db.collection("tasks").document(id)
+                .update(task)
+                .addOnSuccessListener(aVoid -> {
+                    Toast.makeText(this, "Tarefa atualizada com sucesso!", Toast.LENGTH_SHORT).show();
+
+                    finish();
+                })
+                .addOnFailureListener(e -> {
+                    Toast.makeText(this, "Erro ao atualizar tarefa: " + e.getMessage(), Toast.LENGTH_SHORT).show();
+                });
     }
 
     private void datePicker() {
@@ -75,5 +108,18 @@ public class UpdateTask extends AppCompatActivity {
                 binding.data.setText(formattedDate);
             });
         });
+    }
+
+    private void updateFields() {
+        String disciplina = intent.getStringExtra("disciplina");
+        String titulo = intent.getStringExtra("titulo");
+        String dataDeEntrega = intent.getStringExtra("data");
+        String descricao = intent.getStringExtra("descricao");
+        id = intent.getStringExtra("id");
+
+        binding.data.setText(dataDeEntrega);
+        binding.disciplina.setText(disciplina);
+        binding.titulo.setText(titulo);
+        binding.descricao.setText(descricao);
     }
 }
